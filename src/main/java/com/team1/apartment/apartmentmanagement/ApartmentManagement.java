0d25efda2fe249bb17efe4d86f1ae6e0e8e0df7f
Phan Trọng Thoại <138378089+pt-T20-10@users.noreply.github.com>
2026-01-08 20:33:38 +0700

    Khoi tao du an

---
+++ src/main/java/com/team1/apartment/apartmentmanagement/ApartmentManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.team1.apartment.apartmentmanagement;
+
+/**
+ *
+ * @author acer
+ */
+public class ApartmentManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
